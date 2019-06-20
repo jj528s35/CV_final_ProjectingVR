007932d92794ac6bb7a79ccc55fca7ae5cea509b
YuanSyun <rty3497374@gmail.com>
2019-06-20 19:24:44 +0800

    for debug

--- socket_sender.py
+++ socket_sender.py
@@ -1,20 +1,21 @@
-import socket, sys
+import socket
 
 port = 7777
 sock = None
+sock_state = 0
 
 def create_socket():
     '''
         create a sock
     '''
-    global sock, port
+    global sock, port, sock_state
 
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect(('127.0.0.1', port))
     except socket.error as msg:
         print("[ERROR] %s\n"%(msg))
-        exit(1)
+        sock_state = -1
     print('create socket successful')
 
 def close_socket():
@@ -32,11 +33,12 @@ def send(string):
         send the data with TCP sock.
         data format: "data_type data[0] data[1] ..."
     '''
-    global sock
-    if sock is None:
-        create_socket()
-    else:
-        sock.sendall(string.encode())
+    global sock, sock_state
+    if(sock_state != -1):
+        if sock is None:
+            create_socket()
+        else:
+            sock.sendall(string.encode())
 
 if(__name__ == '__main__'):
     create_socket()
